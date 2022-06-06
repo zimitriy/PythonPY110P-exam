39595523dddbce470377e49b1fec1ea56e9ea40f
zimitriy <106424580+zimitriy@users.noreply.github.com>
2022-06-06 13:08:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,79 @@
-from faker import Factory
+import json
+from random import randint
+from random uniform
 from faker import Faker
-fake1 = Factory.create()
 from conf import MODEL
-import random
 
-title = list(open('books.txt'))
-
-def books():
-    fields = {
-        'title': random.choice(title).rstrip(),
-        'year': random.randrange(1900, 2023),
-        'pages': random.randrange(1000),
-        'isbn13': fake1.isbn13(),
-        'rating': round(random.uniform(0.0, 5.0), 1),
-        'price': round(random.uniform(0, 1000), 2),
-        'author': fake1.name()
-    }
-    # return {key: value for key, value in fields.items()}
-    for key, value in fields.items():
-        print(f'{key}-{value}')
+pk = 1
 
 
-print(books())
+def get_title():
+    with open("books.txt", "r") as file:
+        titles = file.read().split("\n")
+        return titles[randint(0, len(titles) - 1)]
+
+
+def get_year():
+    return randint(0, 2023)
+
+
+def get_pages():
+    return randint(0, 1000)
+
+
+def get_isbn():
+    fake = Faker()
+    return fake.isbn13()
+
+
+def get_rating():
+    return round(uniform(0, 5.0), 1)
+
+
+def get_price():
+    return round(uniform(0, 1000), 1)
+
+
+def get_author():
+    fake = Faker()
+    Faker.seed()
+    authors = [fake.name() for i in range(randint(0, 3))]
+    return authors
+
+
+def get_pk():
+    global pk
+    old_state = pk
+    pk += 1
+    return old_state
+
+
+def generate_books(count, init_pk=None):
+    global pk
+    if init_pk is not None:
+        pk = init_pk
+    books = []
+    for i in range(count):
+        book = {"model": MODEL, "pk": get_pk()}
+        fields = {}
+        fields.update({"title": get_title()})
+        fields.update({"year": get_year()})
+        fields.update({"pages": get_pages()})
+        fields.update({"isbn13": get_isbn()})
+        fields.update({"rating": get_rating()})
+        fields.update({"price": get_price()})
+        fields.update({"author": get_author()})
+        book.update({"fields": fields})
+        books.append(book)
+    return books
+
+
+def main():
+    books = generate_books(100)
+    with open("result.json", "w") as json_file:
+        json_string = json.dumps(books, indent=3)
+        json_file.write(json_string)
+
+
+if __name__ == '__main__':
+    main()
